bbd1650bd931acf7a33a1808764551e950f31639
dragonGR <alex@dragongr.dev>
2023-07-21 15:41:27 +0300

    PyCipher: Update Password Generation Code
    This commit updates the password generation code to improve security, readability, and user experience.
    The changes include using the secrets module for cryptographically secure random number generation,
    and refactoring the code to make it more modular and easier to maintain.
    The ambiguous characters to be excluded from passwords have been updated to include 'l', '1', 'I', 'i', 'O', 'o', and '0'.
    Additionally, default values are now used for password length and character inclusion, and input validation
    has been enhanced to handle invalid inputs gracefully.
    These changes aim to provide a safer and more user-friendly password generation process.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import random
 import string
+import secrets
 
-def generate_password(length=12, include_digits=True, include_symbols=True, exclude_ambiguous=True):
+# Constants
+DEFAULT_PASSWORD_LENGTH = 12
+DEFAULT_INCLUDE_DIGITS = True
+DEFAULT_INCLUDE_SYMBOLS = True
+DEFAULT_EXCLUDE_AMBIGUOUS = True
+
+def generate_password(length=DEFAULT_PASSWORD_LENGTH, include_digits=DEFAULT_INCLUDE_DIGITS, include_symbols=DEFAULT_INCLUDE_SYMBOLS, exclude_ambiguous=DEFAULT_EXCLUDE_AMBIGUOUS):
     characters = string.ascii_letters
     if include_digits:
         characters += string.digits
@@ -9,11 +15,11 @@ def generate_password(length=12, include_digits=True, include_symbols=True, excl
         characters += string.punctuation
 
     if exclude_ambiguous:
-        ambiguous_characters = 'l1O0'
+        ambiguous_characters = 'l1IiL0Oo'
         characters = ''.join(c for c in characters if c not in ambiguous_characters)
 
     try:
-        password = ''.join(random.choice(characters) for _ in range(length))
+        password = ''.join(secrets.choice(characters) for _ in range(length))
         return password
     except ValueError:
         print("Error: Invalid password length.")
@@ -21,17 +27,21 @@ def generate_password(length=12, include_digits=True, include_symbols=True, excl
 
 def main():
     try:
-        length = int(input("Enter the desired password length: "))
-        include_digits = input("Include digits (y/n): ").lower() == 'y'
-        include_symbols = input("Include symbols (y/n): ").lower() == 'y'
-        exclude_ambiguous = input("Exclude ambiguous characters (e.g., 'l', '1', 'O', '0') (y/n): ").lower() == 'y'
+        length = int(input("Enter the desired password length (default is {}): ".format(DEFAULT_PASSWORD_LENGTH)))
+
+        if length <= 0:
+            raise ValueError("Invalid input. Password length must be a positive integer.")
+
+        include_digits = input("Include digits (y/n, default is {}): ".format("y" if DEFAULT_INCLUDE_DIGITS else "n")).lower() == 'y'
+        include_symbols = input("Include symbols (y/n, default is {}): ".format("y" if DEFAULT_INCLUDE_SYMBOLS else "n")).lower() == 'y'
+        exclude_ambiguous = input("Exclude ambiguous characters (e.g., 'l', '1', 'I', 'i', 'O', 'o', '0') (y/n, default is {}): ".format("y" if DEFAULT_EXCLUDE_AMBIGUOUS else "n")).lower() == 'y'
 
         password = generate_password(length, include_digits, include_symbols, exclude_ambiguous)
 
         if password:
             print("Generated Password:", password)
-    except ValueError:
-        print("Error: Invalid input. Please enter a valid length.")
+    except ValueError as ve:
+        print("Error:", str(ve))
     except KeyboardInterrupt:
         print("\nPassword generation interrupted.")
 
